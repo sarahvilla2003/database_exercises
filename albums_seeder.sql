a354b00324e433c19955310da7997f6d4345e85d
SarahVillareal <sarahvilla2003@yahoo.com>
2017-05-31 13:43:32 -0500

    Creating Albums table

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+INSERT INTO albums (id, artist, name, release_date, sales, genre)
+VALUES ('', 'Michael Jackson', 'Thriller', '1982', '46.3','Pop, Rock, R&B'),
+  ('', 'AC/DC', 'Back in Black', '1980', '26.1','Hard Rock'),
+  ('', 'Pink Floyd', 'The Dark Side of the Moon', '1982', '46.3','Pop, Rock, R&B'),
+  ('', 'Whitney Houston', 'The Bodyguard', '1982', '46.3','Pop, Rock, R&B'),
+  ('', 'Meat Loaf', 'Bat Out of Hell', '1982', '46.3','Pop, Rock, R&B'),
+  ('', 'Eagles', 'Their Greatest Hits (1971-1975)', '1982', '46.3','Pop, Rock, R&B'),
+  ('', 'Bee Gees', 'Saturday Night Fever', '1982', '46.3','Pop, Rock, R&B'),
+  ('', 'Fleetwood Mac', 'Rumours', '1982', '46.3','Pop, Rock, R&B'),
+
